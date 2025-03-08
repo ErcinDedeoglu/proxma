ded2ed5dc1ef7bb365b49307cfabd7907e5e19d0
ErcinDedeoglu <e.dedeoglu@gmail.com>
2025-03-08 11:06:48 +0400

    feat(nginx_queue_processor.rs): add option to skip certificate request
    Add a condition to check if the message has the skip_certification flag
    set. This allows skipping the certificate request process for specific
    domains, providing more flexibility in handling domain configurations.

--- src/queue/nginx_queue_processor.rs
+++ src/queue/nginx_queue_processor.rs
@@ -36,8 +36,13 @@ impl NginxQueueProcessor {
                             match NGINX_MANAGER.reload_nginx() {
                                 Ok(_) => {
                                     println!("✅ Nginx configuration reloaded successfully");
-                                    CertEnqueue::message(host.domain.clone(), message.clone());
-                                    print!("🔒 Certificate request queued for '{}'", host.domain);
+                                    if !message.skip_certification {
+                                        CertEnqueue::message(host.domain.clone(), message.clone());
+                                        print!("🔒 Certificate request queued for '{}'", host.domain);
+                                    }
+                                    else {
+                                        println!("🔒 Skipping certificate request for '{}'", host.domain);
+                                    }
                                 },
                                 Err(e) => eprintln!("❌ Failed to reload nginx: {}", e),
                             }
